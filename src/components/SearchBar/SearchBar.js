d334dd78b06d8f13ea665b3d9e7f78782cd69a34
slroberts <32742307+slroberts@users.noreply.github.com>
2017-11-16 22:02:27 -0500

    The third part of the Ravenous project
    Added visual feedback for the sorting options at the top of the search bar
    Set the state of sorting options and input elements
    Simulated a search query with the "Let's Go" button

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -12,11 +12,61 @@ class SearchBar extends React.Component {
   //the list items needed to display the sort options
   //(Best Match, Highest Rated, Most Reviewed).
   //This is to help future proof against potential changes to the Yelp API.
+  constructor(props) {
+
+      super(props);
+      this.state = {
+        term : '',
+        location : '',
+        sortBy : 'best_match'
+      };
+
+      this.handleTermChange = this.handleTermChange.bind(this);
+      this.handleLocationChange = this.handleLocationChange.bind(this);
+      this.handleSearch = this.handleSearch.bind(this);
+  }
+
   renderSortByOptions() {
+
     return Object.keys(sortByOptions).map(sortByOption => {
       let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+      return (
+        <li className={this.getSortByClass(sortByOptionValue)} onClick={this.handleSortByChange.bind(this, sortByOptionValue)} key={sortByOptionValue}>
+          {sortByOption}
+          </li>
+      );
     });
+  }
+
+  getSortByClass(sortByOption) {
+    if( this.state.sortBy === sortByOption ) {
+      return 'active';
+    } else {
+        return '';
+    }
+  }
+
+  handleSortByChange(sortByOption) {
+    this.setState(
+      { sortBy : sortByOption }
+    );
+  }
+
+  handleTermChange(event) {
+    this.setState(
+      { term: event.target.value }
+    );
+  }
+
+  handleLocationChange(event) {
+    this.setState(
+      { location: event.target.value }
+    );
+  }
+
+  handleSearch(event) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+    event.preventDefault();
   }
 
   render(){
@@ -28,10 +78,10 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
+          <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+          <input onChange={this.handleLocationChange} placeholder="Where?" />
         </div>
-        <div className="SearchBar-submit">
+        <div className="SearchBar-submit" onClick={this.handleSearch}>
           <a>Let&#39;s Go</a>
         </div>
       </div>
